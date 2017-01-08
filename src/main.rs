334c8b034ddf83f66ecf9b5f87858fb2ad56c20a
Ned Pummeroy <nedpummeroy@gmail.com>
2017-01-08 18:51:25 +1100

    Improve documentation

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use vec_map::VecMap;
 
 const PATH: &'static str = "./.ado/";
 
+/// Constructs the application and runs the GUI.
 fn main() {
     let todo_list = FileTodoList::new().unwrap();
     let mut task_picker = TaskPicker {
@@ -22,39 +23,55 @@ fn main() {
         tasks: todo_list,
     };
 
-    gui(&mut task_picker).unwrap();
+    gui(&mut task_picker);
 }
 
-fn gui<T>(task_picker: &mut TaskPicker<T>) -> Result<(), Error>
+/// Handles input and output for the lifetime of the application.
+///
+/// The function initialises ncurses and the screen, then in a loop:
+///
+/// 1. updates the screen and
+/// 2. handles user input.
+///
+/// This function returns when the user enters a quit command.
+///
+/// Partially completed commands are shown, but not complete
+/// or invalid commands.
+/// e.g. pressing 'd' will cause d to be printed at the bottom
+/// of the screen until the command is completed (e.g. as 'dd')
+/// or abandoned.
+fn gui<T>(task_picker: &mut TaskPicker<T>)
     where T: TodoList<Id = usize, Error = Error>
 {
-    use std::error::Error;
+    use ::std::error::Error;
 
     ::ncurses::initscr();
     ::ncurses::curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
     ::ncurses::noraw();
     ::ncurses::cbreak();
 
-    let mut error: Option<self::Error> = None;
+    // Print the initial state of the task picker.
+    ::ncurses::clear();
+    ::ncurses::printw(&format!("{}\n", task_picker));
+    ::ncurses::refresh();
 
     loop {
-        ::ncurses::clear();
-        ::ncurses::printw(&format!("{}\n", task_picker));
-        if let Some(err) = error {
-            ::ncurses::printw(&format!("{}\n", err.description()));
-        }
-        ::ncurses::refresh();
-
+        // Handle user input, and store any errors which are produced.
+        // Generate a new error if the input is unrecognised.
         let result = match ::ncurses::getch() {
             x => {
                 match char::from(x as u8) {
+                    // Quit on q.
                     'q' => break,
 
+                    // Basic movement commands.
                     'h' => task_picker.left(),
                     'j' => task_picker.down(),
                     'k' => task_picker.up(),
                     'l' => task_picker.right(),
 
+                    // Get a new task name from the user and use the
+                    // name to create a new task.
                     'o' => {
                         ::ncurses::printw("\nEnter a task summary:\n");
                         ::ncurses::curs_set(CURSOR_VISIBILITY::CURSOR_VISIBLE);
@@ -66,6 +83,7 @@ fn gui<T>(task_picker: &mut TaskPicker<T>) -> Result<(), Error>
                         task_picker.create(name).map(|_| ())
                     }
 
+                    // Long distance scrolling.
                     'G' => task_picker.bottom(),
                     'g' => {
                         match char::from(::ncurses::getch() as u8) {
@@ -74,6 +92,7 @@ fn gui<T>(task_picker: &mut TaskPicker<T>) -> Result<(), Error>
                         }
                     }
 
+                    // Task deletion.
                     'D' => task_picker.remove(),
                     'd' => {
                         match char::from(::ncurses::getch() as u8) {
@@ -87,11 +106,17 @@ fn gui<T>(task_picker: &mut TaskPicker<T>) -> Result<(), Error>
             }
         };
 
-        error = result.err();
+        // Print the state of the task picker as well printing
+        // any required error messages.
+        ::ncurses::clear();
+        ::ncurses::printw(&format!("{}\n", task_picker));
+        if let Err(err) = result {
+            ::ncurses::printw(&format!("{}\n", err.description()));
+        }
+        ::ncurses::refresh();
     }
 
     ::ncurses::endwin();
-    Ok(())
 }
 
 struct TaskPicker<T> {
@@ -266,7 +291,7 @@ impl Display for Error {
     }
 }
 
-impl std::error::Error for Error {
+impl ::std::error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::AlreadyDone => "The task is already finished",
@@ -478,33 +503,14 @@ impl TodoList for FileTodoList {
 fn ids() -> Result<Box<Iterator<Item = usize>>, ::std::io::Error> {
     let read_dir = ::std::fs::read_dir(PATH)?;
 
-    // FIXME: ADO-01 report errors in some way.
-    let mut ids = read_dir
-        // Convert each result to entry
-        .flat_map(Result::ok)
-        // Convert each entry to filename; use autoref.
+    // TODO: Report errors in some way.
+    // Get a usize for each filename in the data path, where possible.
+    let mut ids = read_dir.flat_map(Result::ok)
         .map(|entry| entry.file_name())
-        // Convert each filename to string
         .flat_map(OsString::into_string)
-        // Parse id; use autoref.
         .flat_map(|name| name.parse())
-
         .collect::<Vec<_>>();
+
     ids.sort();
     Ok(Box::new(ids.into_iter()))
-}
-
-impl Display for FakeTodoList {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let list_format = self.tasks
-            .values()
-            .filter(|task| match task.status {
-                Status::Wont => false,
-                _ => true,
-            })
-            .map(|task| format!("{}", task))
-            .collect::<Vec<_>>()
-            .join("\n");
-        write!(f, "{}", list_format)
-    }
 }
